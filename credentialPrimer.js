8db85c9f7a6f312c9f71d03e1c8b82efae6e161c
Travis Savo <evilgenius@nefariousplan.com>
2017-01-31 15:13:12 -0800

    Introducing mongo and proper classes. Hooray for grown up coding styles!

---
+++ credentialPrimer.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const fs = require("fs");
+const mp = require("mongodb");
+var credentials = JSON.parse(fs.readFileSync(".credentials.json", {encoding: "UTF-8"}));
+mp.MongoClient.connect(credentials.mongodb).then((db) => {
+
+    return db.collection("credentials").insertOne(
+        {
+        }
+    ).then(() => {
+        db.close();
+    });
+}).catch(console.log);
